426be989ec034847bc9fcb0ceedae5b7c8d7b726
Ryan <crdunaway@gmail.com>
2017-12-11 10:58:10 -0500

    Initial commit. The way I like it so far :)

---
+++ js/custom.js
@@ -0,0 +1,56 @@
+var map;
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navBarHeight = $('header').outerHeight();
+
+$('#sidebarCollapseBtn').on('click', function () {
+    var sidebar = $('#sidebar');
+    var $body = $('body');
+    $body.toggleClass('noscroll');
+    sidebar.toggleClass('opened');
+    $(this).toggleClass('active');
+    sidebar.css({'top':lastScrollTop});
+});
+
+$(window).scroll(function(){
+    didScroll = true;
+});
+
+
+function initMap() {
+    var uluru = {lat: 36.1014662, lng: -80.243257};
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
+function hasScrolled() {
+    var myHeader = $('header');
+    var st = $(this).scrollTop();
+    if (Math.abs(lastScrollTop-st) <= delta) return;
+    if (st > lastScrollTop && st > navBarHeight) {
+        if (!myHeader.hasClass('nav-up')){
+            myHeader.addClass('nav-up');
+        }
+    } else {
+        if (st + $(window).height() < $(document).height()){
+            if(myHeader.hasClass('nav-up')){
+                myHeader.removeClass('nav-up');
+            }
+        }
+    }
+
+    lastScrollTop = st;
+}
+
+setInterval(function(){
+    if(didScroll){
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
